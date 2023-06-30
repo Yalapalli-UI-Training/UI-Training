8e244ac778273294b6ab8f0675f04e5d091e398a
Nikith <nikith1212.n@gmail.com>
2023-06-30 21:07:33 +0530

    app.js-practice var-Let-Const

---
+++ nikith_app.js
@@ -0,0 +1,135 @@
+// console.log("Hellow Siva");
+
+
+//  var greeter = "hey Hi";
+//   function newfunction() {
+//     var hello = "hello";
+//     console.log(hello);
+
+
+//   }
+
+//   newfunction();
+  
+
+//  // Three types of variables
+
+//  // Variable declariation with     "Var"
+
+//  // with var we can re declar and updated
+// // Eeample of re-declearation:-
+
+// var greetings = "Good Mornming";
+// console.log(greetings);
+// var greetings = "Good Afternoon";
+// console.log(greetings);
+// var greetings = "Good 9t";
+// console.log(greetings);
+
+// // Example of update 
+
+// var greetings = "world";
+// console.log(greetings);
+// greetings = "Water";
+// console.log(greetings);
+
+// // variable decleariation with Let :- we cant re declar but we can update
+
+// let cityName = "Tirupathi";
+// console.log(cityName);
+// cityName = "HYB";
+// console.log(cityName);
+
+// // variable decleariation with Const :- we cannot redeclare and we cannot update
+
+// const ganga = "River";
+// console.log(ganga);
+// ganga = "ocean";
+
+
+// with var we can declear and we can update.
+
+var greetings = "Hi nani";
+console.log(greetings);
+
+// Update of var
+
+greetings ="Hi Nikith";
+console.log(greetings);
+
+//Let :-  we  cant re-declare , but we can update.
+
+let ourCountry = "India"
+console.log(ourCountry);
+
+//Update of let
+
+ourCountry = "Africa";
+console.log(ourCountry);
+
+// we cannot redeclare and we cant update
+
+const  myName = "Nikith";
+console.log(myName);
+myName = "Chinna";
+console.log(myName);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
